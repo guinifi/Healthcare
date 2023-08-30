693c6c9490811160823b2806420063ee22752a95
David <david.taitague@hotmail.com>
2023-08-30 16:09:56 -0700

    Deleted unnecessary files

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+// Initialize dotenv for environment variables
 require('dotenv').config();
 
+// Import required modules
 const { CosmosClient } = require("@azure/cosmos");
 
+// Retrieve environment variables for CosmosDB connection
 const endpoint = process.env.COSMOS_DB_ENDPOINT;
 const key = process.env.COSMOS_DB_KEY;
 
+// Initialize Cosmos DB client
 const client = new CosmosClient({ endpoint, key });
 
-console.log('Cosmos DB Client Initialized');
+// Log a message to indicate successful initialization
+console.log('Azure Cosmos DB client has been initialized successfully.');
+
