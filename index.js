46456db066e177b3ef67fccade53185bdbf5d7f3
David <david.taitague@hotmail.com>
2023-08-30 18:32:21 -0700

    Updated files

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
 // Initialize dotenv for environment variables
 require('dotenv').config();
 
@@ -5,7 +9,7 @@ require('dotenv').config();
 const { CosmosClient } = require("@azure/cosmos");
 
 // Retrieve environment variables for CosmosDB connection
-const endpoint = process.env.COSMOS_DB_ENDPOINT;
+const endpoint = process.env.COSMOS_DB_ENDPOINT || "https://healthcaredb.documents.azure.com:443/";
 const key = process.env.COSMOS_DB_KEY;
 
 // Initialize Cosmos DB client
@@ -14,3 +18,10 @@ const client = new CosmosClient({ endpoint, key });
 // Log a message to indicate successful initialization
 console.log('Azure Cosmos DB client has been initialized successfully.');
 
+// Serve static files from a "public" directory
+app.use(express.static('public'));
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
