8eb0d8f6519386f18501be5553e5d11faadcc2b6
David <david.taitague@hotmail.com>
2023-08-30 19:30:11 -0700

    Added more logging for debugging

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
+// existing imports
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;  // Changed this line
+const port = process.env.PORT || 3000;
 
 // Initialize dotenv for environment variables
 require('dotenv').config();
 
-// Import required modules
+// Add a log to check the PORT value
+console.log(`Listening on port: ${port}`);
+
+// existing code
 const { CosmosClient } = require("@azure/cosmos");
 
-// Retrieve environment variables for CosmosDB connection
 const endpoint = process.env.COSMOS_DB_ENDPOINT || "https://healthcaredb.documents.azure.com:443/";
 const key = process.env.COSMOS_DB_KEY;
 
-// Initialize Cosmos DB client
 const client = new CosmosClient({ endpoint, key });
 
-// Log a message to indicate successful initialization
 console.log('Azure Cosmos DB client has been initialized successfully.');
 
-// Serve static files from a "public" directory
 app.use(express.static('public'));
 
-// Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
+
 
